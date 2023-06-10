37678c0a0c3384a9b12e9e649e542299321b82ae
Sam Nixon samnixon87@gmail.com <samnixon87@gmail.com>
2023-06-10 10:30:04 +0100

    Mini refactor

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -8,22 +8,24 @@ function App() {
 
   const [token, setToken] = useState<string | null>(null)
 
+  const clientId = import.meta.env.VITE_CLIENT_ID;
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get("code");
+
   useEffect(() => {
-    getToken()
+    if(code && !token) {
+      getToken()
+    }
   }, [])
 
-  console.log(token)
-
   const getToken = async () => {
-    const clientId = import.meta.env.VITE_CLIENT_ID;
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get("code");
-
     if(code){
       const accessToken = await getAccessToken(clientId, code)
       setToken(accessToken)
     }
   }
+
+  
 
   if (!token) {
     return(
